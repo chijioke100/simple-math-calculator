0a4a791971dc83e048156db237d596cd376e2f3c
Chijioke Philip Ogbonnaya <chijiokee31@gmail.com>
2024-11-07 10:31:56 +0000

    add event listeners for button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', function() {
+            if (this.getAttribute('data-type') === 'calculate') {
+                alert('You clicked calculate'); 
+            } else {
+                let calcType = this.getAttribute('data-type');
+                alert(`You clicked ${calcType}`);
+            }
+        });
+    }
+});
+
 function executeCalc() {
 
 }
